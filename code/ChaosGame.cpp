59ff54f1400fb7084eef1ce10ea97bc2455101e9
Zeriø <85314670+Zerio105@users.noreply.github.com>
2025-04-22 14:43:14 -0700

    Gradient background

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -14,7 +14,7 @@ int main()
 	// Create a video mode object
 	VideoMode vm(1920, 1080);
 	// Create and open a window for the game
-	RenderWindow window(vm, "Chaos Game!!", Style::Default);
+	RenderWindow window(vm, "Chaos Game!!", Style::Fullscreen);
 	
 	vector<Vector2f> vertices;
 	vector<Vector2f> points;
@@ -78,14 +78,35 @@ int main()
 		****************************************
 		*/
 		window.clear();
+
+		// Draw gradient background
+		Color BG1(100, 150, 180);
+		Color BG2(0, 0, 0);
+		const int BG_STRIP_HEIGHT = 10;
+
+		Color ColorStep(((BG1.r - BG2.r) / (vm.height / BG_STRIP_HEIGHT)), ((BG1.g - BG2.g) / (vm.height / BG_STRIP_HEIGHT)), ((BG1.b - BG2.b) / (vm.height / BG_STRIP_HEIGHT)));
+
+		for (int i = 0; i < vm.height / BG_STRIP_HEIGHT; i++)
+		{
+			RectangleShape testLine(Vector2f(vm.width, i * BG_STRIP_HEIGHT));
+			testLine.setPosition(Vector2f(0, i * BG_STRIP_HEIGHT));
+			testLine.setFillColor(BG1);
+			window.draw(testLine);
+			BG1 += ColorStep;
+		}
+
+		// Draw User-defined Points
 		for(int i = 0; i < vertices.size(); i++)
 		{
-		    RectangleShape rect(Vector2f(10,10));
+			RectangleShape rect(Vector2f(10,10));
 		    rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
-		    rect.setFillColor(Color::Blue);
-		    window.draw(rect);
+			rect.setFillColor(Color::Blue);
+			window.draw(rect);
 		}
+		
 		///TODO:  Draw points
+
+
 		window.display();
 	}
 }
