9779b4f794469bd8ee4a30b81a8fa05d7c3c7f55
Zerio <Zerio105@gmail.com>
2025-04-22 15:15:20 -0700

    Rotate points

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -101,6 +101,7 @@ int main()
 			RectangleShape rect(Vector2f(10,10));
 		    rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
 			rect.setFillColor(Color::Blue);
+			rect.rotate(45);
 			window.draw(rect);
 		}
 		
