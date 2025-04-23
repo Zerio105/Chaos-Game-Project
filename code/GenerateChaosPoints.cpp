ec0b75757ba289956a10f852fb61a51e7ccb871f
Zeriø <85314670+Zerio105@users.noreply.github.com>
2025-04-23 14:08:49 -0700

    Delete code/GenerateChaosPoints.cpp

--- code/GenerateChaosPoints.cpp
+++
@@ -1,9 +0,0 @@
-// Generate new points per drawn frame for active Chaos Game
-
-if (points.size() > 0)
-{
-	///generate more point(s)
-	///select random vertex
-	///calculate midpoint between random vertex and the last point in the vector
-	///push back the newly generated coord.
-}
